c34f2645c84237d0d2f8314a53ce59325ee91adf
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-05-01 17:05:24 +0500

    HomeWork 1

---
+++ app/src/main/java/com/bsef19m517/hw/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bsef19m517.hw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.layout_calculator);
+        //setContentView(R.layout.layout_todo);
+    }
+}
