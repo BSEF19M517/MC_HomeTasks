8a7655741422753fcf6fe46ab04a630dea33236f
BSEF19M517 <bsef19m517@pucit.edu.pk>
2022-05-24 09:23:53 +0500

    Home Screen

---
+++ HW_02/app/src/main/java/com/bsef19m517/alphabets/LessonOne.java
@@ -0,0 +1,14 @@
+package com.bsef19m517.alphabets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LessonOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lesson_one);
+    }
+}
